6524852f19726e9b6e21e27417b756e3e5792438
kzorin52 <kzorin52@gmail.com>
2021-01-01 17:02:30 +0600

    1.8 upd

--- Program.cs
+++ Program.cs
@@ -1,129 +1,226 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using ProxyScrapeAPI;
 using xNet;
 
 namespace HideMyNameKeyParser
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var htmls = "";
-            var keys = new List<string>();
+
             var sites = new List<string>
             {
                 "https://vk.com/hidemy_vpn_keys",
                 "https://vk.com/hidemy_name_keys",
-                "https://vk.com/keys_hidemy_vpn"
+                "https://vk.com/keys_hidemy_vpn",
+                "https://t.me/s/hidemyname_keys",
+                "https://t.me/s/HideMyNameKeys",
+                "https://t.me/s/freeHideMyVpn"
             };
 
-            Console.Title = "\tHideMyName code parser & checker by -=[TEMNIJ]=- v 1.0";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
+                                                   SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
 
+            Console.Title = "\tHideMyName code parser & checker by -=[TEMNIJ]=- v 1.8";
+            var keys = new List<string>();
 
-            #region Parser
-
-            foreach (var site in sites)
+            Console.WriteLine("Режим:\n\t1 - Генерация & чек\n\t2 - Парсинг & чек\n");
+            var mode = int.Parse(Console.ReadLine());
+            if (mode == 1)
             {
-                Log("Downloading " + site);
-                htmls += new WebClient().DownloadString(site) + "\n";
-                Log("\t~Downloaded~\n");
+                #region Generator
 
-                if (sites.IndexOf(site) != 2) Thread.Sleep(3000);
+                for (var i = 0; i < 50; i++)
+                {
+                    var l1 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l2 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l3 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l4 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l5 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l6 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l7 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l8 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l9 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l10 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l11 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l12 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l13 = new Random(Environment.TickCount).Next(0, 9).ToString();
+                    Thread.Sleep(5);
+                    var l14 = new Random(Environment.TickCount).Next(0, 9).ToString();
+
+                    keys.Add(l1 + l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9 + l10 + l11 + l12 + l13 + l14);
+                }
+
+                #endregion Generator
             }
 
-            foreach (Match match in Regex.Matches(htmls, "(\\d{14})"))
+            if (mode == 2)
             {
-                Log(match.Groups[1]);
-                keys.Add(match.Groups[1].Value);
+                #region Parser
+
+                foreach (var site in sites)
+                {
+                    Log("Downloading " + site);
+                    htmls += new WebClient().DownloadString(site) + "\n";
+                    Log("\t~Downloaded~\n");
+
+                    if (sites.IndexOf(site) != 2) Thread.Sleep(3000);
+                }
+
+                keys.AddRange(Regex.Matches(htmls, "(\\d{14})")
+                    .OfType<Match>()
+                    .Select(m => m.Groups[0].Value)
+                    .ToArray());
+                Log($"Нашёл {keys.Count} ключа(ей)!");
+
+                #endregion
             }
-
-            #endregion
-
-            #region Checker
-
-            Console.ForegroundColor = ConsoleColor.Red;
-
-            Log("\nChecking...\n\n");
-
-            var req = new HttpRequest();
-
-            Log("Чел. Откуда прокси брать будем? Принимаю только Socks4 пока!");
-
-            var proxies = File.ReadAllLines(Console.ReadLine());
-
-            var x = 0;
-
-            keys.Shuffle();
 
             var okKeysList = new List<string>();
 
-            foreach (var key in keys)
+            var check = new Task(async () =>
             {
-                await Task.Run(() =>
+                #region Checker
+
+                
+                void SaveAndGrabProxies(object obj)
                 {
-                    check:
-                    try
-                    {
-                        req.Proxy = ProxyClient.Parse(ProxyType.Socks4, proxies[x]);
-                    }
-                    catch (IndexOutOfRangeException)
-                    {
-                        x = 0;
-                    }
+                    File.WriteAllLines($"Keys-{DateTime.Now:dd-MM-yyyy-HH-mm}.txt", okKeysList);
+                    Log("\nСохранил\n");
+                }
 
-                    try
-                    {
-                        var request =
-                            req.Get($"https://hidemy.name/api/vpn_check_code.php?code={key}").ToString();
+                var timer = new Timer(SaveAndGrabProxies, 0, 1000 * 60 * 5, 1000 * 60 * 5);
 
-                        if (request.Contains("PROXY"))
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Log("\nChecking...\n\n");
+
+                var req = new HttpRequest();
+
+                Log("Чтоб облегчить тебе труд, так уж и быть, прокси я сам скачаю");
+
+                var proxies = new Scraper()
+                    .GetProxies(Scraper.ProxyType.Socks4, 1500)
+                    .Replace("\r", "")
+                    .Split('\n');
+
+                Log($"\tСкачал {proxies.Length} проксей \n");
+                Log("Чтобы выйти, смело жми 'e', ну а чтобы сохранить все 'OK' ключи, жми 's'!\n");
+
+                var x = 0;
+
+                keys.Shuffle();
+
+
+                foreach (var key in keys)
+                {
+                    await Task.Run(() =>
+                    {
+                        check:
+                        try
+                        {
+                            req.Proxy = ProxyClient.Parse(ProxyType.Socks4, proxies[x]);
+                            req.UserAgent = "AndroidVPNGui/2.0.79 (h-RU; Android SDK: 29 (10); ru; +0600)";
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            x = 0;
+                            goto check;
+                        }
+                        catch (ArgumentException)
                         {
                             x++;
                             goto check;
                         }
 
-                        if (request.Contains("FAST"))
+                        try
                         {
-                            Thread.Sleep(500);
+                            var request =
+                                req.Get($"https://hidemy.name/api/vpn_check_code.php?out=js&code={key}").ToString();
+
+                            var code = JObject.Parse(request)["result"].ToString();
+
+                            if (code.Contains("PROXY"))
+                            {
+                                x++;
+                                goto check;
+                            }
+
+                            if (code.Contains("FAST"))
+                            {
+                                Thread.Sleep(500);
+                                x++;
+                                goto check;
+                            }
+
+                            if (code.Contains("OK"))
+                            {
+                                okKeysList.Add(key);
+
+                                Log(key + " " + code + " " +
+                                    JObject.Parse(request)["time_remaining"].ToString().ToInt() / 60 / 60 + "h");
+                            }
+                            else
+                            {
+                                Log(key + " " + code);
+                            }
+                        }
+                        catch
+                        {
                             x++;
                             goto check;
                         }
+                    });
 
-                        if (request.Contains("OK")) okKeysList.Add(key);
+                    x++;
+                }
 
-                        Log(key + " " + request);
-                    }
-                    catch
-                    {
-                        x++;
-                        goto check;
-                    }
-                });
+                #endregion
 
-                x++;
-            }
+                #region Timer
 
-            //Log("Done!");
 
-            #endregion
+                #endregion
+            });
+            check.Start();
 
-            var keyC = Console.ReadKey();
-            switch (keyC.Key)
+            while (true)
             {
-                case ConsoleKey.S:
-                    File.WriteAllLines("Keys.txt", okKeysList);
-                    break;
-                case ConsoleKey.E:
-                    Environment.Exit(0);
-                    break;
+                var keyC = Console.ReadKey();
+                switch (keyC.Key)
+                {
+                    case ConsoleKey.S:
+                        File.WriteAllLines($"Keys-{DateTime.Now:dd-MM-yyyy-HH-mm}.txt", okKeysList);
+                        Log("\nСохранил\n");
+                        break;
+                    case ConsoleKey.E:
+                        check.Dispose();
+                        Environment.Exit(0);
+                        break;
+                }
             }
-
-            // Эта фигня почему-то не робит ;((((
         }
 
         private static void Log(object text)
@@ -147,6 +244,11 @@ namespace HideMyNameKeyParser
                 list[k] = list[n];
                 list[n] = value;
             }
+        }
+
+        public static int ToInt(this string stringer)
+        {
+            return int.Parse(stringer);
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HideMyNameKeyParser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Temnij")]
 [assembly: AssemblyProduct("HideMyNameKeyParser")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©Temnij 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8")]
